fc45618b553343a67d7c09403e5396e447bbb7fc
AlexGratner <Gratner@live.se>
2023-07-26 18:03:06 +0200

    Initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About</h1>
+
+	<p>
+	There is no perfect hot sauce.<br>
+	But Alejandros Hot Sauce is damn near perfection.
+	</p>
+	<p>
+	Contact: AlejandrosHotSauce@live.se<br>
+	</p>
+</div>
